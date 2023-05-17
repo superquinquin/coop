63ea7cd6066121f77290fd7ae885e7bcdf3d851d
Ben <ben@nursit.net>
2023-05-17 15:35:06 +0200

    ajouts pauline

---
+++ lang/zcore_fr.php
@@ -0,0 +1,46 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans https://git.spip.net/spip-contrib-extensions/z-core.git
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'accueil' => 'Accueil',
+	'adapte_de' => 'adapté de',
+
+	// C
+	'commentaire' => 'commentaire',
+	'commentaires' => 'commentaires',
+	'conception_graphique_par' => 'Habillage visuel ©',
+	'consulter' => 'Consulter',
+
+	// D
+	'date_forum' => 'Le @date@ à @heure@',
+
+	// I
+	'info_1_commentaire' => '1 commentaire',
+	'info_nb_commentaires' => '@nb@ commentaires',
+
+	// L
+	'lire_la_suite' => 'Lire la suite',
+	'lire_la_suite_de' => ' de ',
+
+	// P
+	'pagination_next' => 'Suivant »',
+	'pagination_pages' => 'Pages',
+	'pagination_prev' => '« Précédent',
+	'personaliser_nav' => 'Personnaliser ce menu',
+
+	// S
+	'sous_licence' => 'sous Licence',
+
+	// V
+	'voir' => 'Voir',
+
+	// Z
+	'zapl_loading' => 'Chargement en cours...',
+	'zapl_reload_off' => 'Cliquer ici si la page reste incomplète (ou activer le javascript dans votre navigateur)'
+);
